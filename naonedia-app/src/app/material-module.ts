41983b75d45dd228baeb359ea09129f4e8f7a745
Thibault Brocherieux <tbrocherieux@ippon.fr>
2019-05-17 16:04:56 +0200

    Added icons & material support

---
+++ naonedia-app/src/app/material-module.ts
@@ -0,0 +1,10 @@
+import {DragDropModule} from '@angular/cdk/drag-drop';
+
+import {NgModule} from '@angular/core';
+
+@NgModule({
+  exports: [
+    DragDropModule,
+  ]
+})
+export class DemoMaterialModule {}
--- naonedia-app/src/app/vendor.ts
+++ naonedia-app/src/app/vendor.ts
@@ -7,9 +7,33 @@ import {
     faBars,
     faFlag,
     faChartBar,
+    faPeopleCarry,
+    faCircle,
+    faTimes,
+    faDatabase,
+    faWalking,
+    faCarSide,
+    faMapMarkerAlt,
+    faSlash,
+    faHome,
+    faBuilding,
 } from '@fortawesome/free-solid-svg-icons';
 
+import { far } from '@fortawesome/free-regular-svg-icons';
+
 // Adds the SVG icon to the library so you can use it in your page
+
 library.add(faBars);
 library.add(faFlag);
 library.add(faChartBar);
+library.add(faPeopleCarry);
+library.add(faCircle);
+library.add(faTimes);
+library.add(faDatabase);
+library.add(faWalking);
+library.add(faCarSide);
+library.add(faMapMarkerAlt);
+library.add(faSlash);
+library.add(faHome);
+library.add(faBuilding);
+library.add(far.faCircle);
