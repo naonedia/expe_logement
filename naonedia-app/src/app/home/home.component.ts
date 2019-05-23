e5266927d74225740a247cd4682c9afc29f28e1d
Thibault Brocherieux <tbrocherieux@ippon.fr>
2019-05-23 15:00:41 +0200

    Added animation

--- naonedia-app/src/app/home/home.component.ts
+++ naonedia-app/src/app/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, AfterContentInit } from '@angular/core';
+import { Component, AfterContentInit, HostListener } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Router } from '@angular/router';
-
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['home.scss']
 })
 export class HomeComponent {
+    cssStyle;
 
-    constructor(private router: Router, private translateService: TranslateService) {}
+    constructor(private router: Router, private translateService: TranslateService, private sanitization: DomSanitizer) {}
+
+    @HostListener('window:scroll', ['$event'])
+    doSomething(event) {
+        const scrolledY = window.pageYOffset;
+        this.cssStyle = this.sanitization.bypassSecurityTrustStyle('background-position : left ' + (scrolledY) + 'px');
+      }
 
     goToModel() {
         this.router.navigate(['model']);
