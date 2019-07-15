b1cfd552678bc17e979254a3cec51e2e97d13fee
zephylac <thibault.brocherieux.etu@univ-lemans.fr>
2019-07-15 11:28:20 +0200

    Updated home page

--- naonedia-app/src/app/home/home.component.ts
+++ naonedia-app/src/app/home/home.component.ts
@@ -2,22 +2,23 @@ import { Component, AfterContentInit, HostListener } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Router } from '@angular/router';
 import { DomSanitizer } from '@angular/platform-browser';
+import { EmbedVideoService } from 'ngx-embed-video';
+
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['home.scss']
 })
 export class HomeComponent {
-    cssStyle;
+    ytIframe: any;
 
-    constructor(private router: Router, private translateService: TranslateService, private sanitization: DomSanitizer) {}
-
-    @HostListener('window:scroll', ['$event'])
-    doSomething(event) {
-        const scrolledY = window.pageYOffset;
-        this.cssStyle = this.sanitization.bypassSecurityTrustStyle('background-position : left ' + (scrolledY) + 'px');
-      }
-
+    constructor(
+        private router: Router,
+        private translateService: TranslateService,
+        private embedService: EmbedVideoService
+    ) {
+        this.ytIframe = this.embedService.embed('https://www.youtube.com/embed/f1xz5KeYmX0?rel=0&amp;showinfo=1');
+    }
     goToModel() {
         this.router.navigate(['model']);
     }
--- naonedia-app/src/app/home/home.module.ts
+++ naonedia-app/src/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { ChatBotSharedModule } from '../shared';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { EmbedVideo } from 'ngx-embed-video';
 
 @NgModule({
-    imports: [ChatBotSharedModule, RouterModule.forChild([HOME_ROUTE])],
+    imports: [ChatBotSharedModule, RouterModule.forChild([HOME_ROUTE]), EmbedVideo.forRoot()],
     declarations: [HomeComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
