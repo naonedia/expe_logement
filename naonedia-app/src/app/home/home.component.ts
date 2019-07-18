8d17766b6c6bdbe8ceacef676690d23e71e3aeb4
zephylac <thibault.brocherieux.etu@univ-lemans.fr>
2019-07-18 15:08:41 +0200

    Removed unused imports

--- naonedia-app/src/app/home/home.component.ts
+++ naonedia-app/src/app/home/home.component.ts
@@ -1,8 +1,6 @@
 import { Component, AfterContentInit, HostListener } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Router } from '@angular/router';
-import { DomSanitizer } from '@angular/platform-browser';
-import { EmbedVideoService } from 'ngx-embed-video';
 
 @Component({
     selector: 'app-home',
@@ -10,15 +8,11 @@ import { EmbedVideoService } from 'ngx-embed-video';
     styleUrls: ['home.scss']
 })
 export class HomeComponent {
-    ytIframe: any;
 
     constructor(
         private router: Router,
         private translateService: TranslateService,
-        private embedService: EmbedVideoService
-    ) {
-        this.ytIframe = this.embedService.embed('https://www.youtube.com/embed/f1xz5KeYmX0?rel=0&amp;showinfo=1');
-    }
+    ) { }
     goToModel() {
         this.router.navigate(['model']);
     }
--- naonedia-app/src/app/shared/shared-libs.module.ts
+++ naonedia-app/src/app/shared/shared-libs.module.ts
@@ -3,13 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { Ng5SliderModule } from 'ng5-slider';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
-    imports: [NgbModule.forRoot(), FontAwesomeModule, Ng5SliderModule, BrowserAnimationsModule, DragDropModule],
-    exports: [FormsModule, CommonModule, NgbModule, FontAwesomeModule, Ng5SliderModule, BrowserAnimationsModule, DragDropModule]
+    imports: [NgbModule.forRoot(), FontAwesomeModule, BrowserAnimationsModule, DragDropModule],
+    exports: [FormsModule, CommonModule, NgbModule, FontAwesomeModule, BrowserAnimationsModule, DragDropModule]
 })
 
 export class ChatBotSharedLibsModule {
